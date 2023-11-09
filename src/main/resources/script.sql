6dffb27771103b1948166c8c12fb6fb0fc7d852c
Александр <alexst731w@gmail.com>
2023-11-09 22:09:53 +0300

    ДЗ по SQL коммит

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,14 +1,14 @@
-SELECT * FROM student;
+SELECT * FROM student
 WHERE age BETWEEN 10 AND 20;
 
 SELECT name FROM student;
 
-SELECT * FROM student;
-WHERE CustomerName LIKE '%a%';
+SELECT * FROM student
+WHERE name LIKE '%a%';
 
-SELECT * FROM student;
+SELECT * FROM student
 WHERE age<id;
 
-SELECT * FROM student;
+SELECT * FROM student
 ORDER BY age;
 
